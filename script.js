6ab6d20de02c99d9c4819758f719c8dee0258c51
Sanat Gupta <62789975+sanatg@users.noreply.github.com>
2023-04-14 06:27:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 function generatePassword() {
-  const length = 18;
+  const length = 16;
   const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-=_+[]{}|;':\",./<>?`~";
   let password = "";
-  
+
   for (let i = 0; i < length; i++) {
     password += charset[Math.floor(Math.random() * charset.length)];
   }
-  
+
   return password;
 }
 
 document.getElementById('generateBtn').addEventListener('click', () => {
   const passwordDisplay = document.getElementById('passwordDisplay');
   const newPassword = generatePassword();
+  
+  // GSAP animation
+  gsap.fromTo(passwordDisplay, {scale: 0.8, opacity: 0}, {scale: 1, opacity: 1, duration: 0.5});
+  
   passwordDisplay.textContent = newPassword;
 });
